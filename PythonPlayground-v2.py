2e52d0a39220f73262bee100e4e0a5132a595a3f
EC2 Default User <ec2-user@ip-172-31-83-212.ec2.internal>
2022-06-22 04:54:19 +0000

    Comments

--- PythonPlayground-v2.py
+++ PythonPlayground-v2.py
@@ -1,2 +1,10 @@
+#!/user/bin/env python3.7
 
-print("Hello World!")
+# This is a full line comment
+
+print("Hello World!") # This is a trailing comment
+
+"""
+This is not a block comment
+There is no such thing in python
+"""
