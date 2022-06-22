c4328dcc7e0b15150d60304ae81d9f4032e0a694
EC2 Default User <ec2-user@ip-172-31-83-212.ec2.internal>
2022-06-22 16:23:35 +0000

    Name, Age and Color Script

---
+++ name_color_age.py
@@ -0,0 +1,5 @@
+Name = input("What is your name? ")
+Color = input("What is your favorite color? ")
+Age = int (input("How old are you today? "))
+
+print (Name, 'is', Age, 'years old and loves the color', Color + '.')
