93555581d9e6fe821d5146d75bd196e198ca5dba
EC2 Default User <ec2-user@ip-172-31-83-212.ec2.internal>
2022-06-29 19:39:27 +0000

    ListComprehensions add

---
+++ ListComprehensionsHR.py
@@ -0,0 +1,10 @@
+x = int(1)
+y = int(1)
+z = int(1)
+n = int(1)
+
+for i in range(x+1):
+    for j in range(y+1):
+        for k in range(z+1):
+            if (i+j+k !=n):
+                print(list([i,j,k]), end = '')
