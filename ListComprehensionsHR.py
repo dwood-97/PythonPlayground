a6cb5d2a363bd9892b598dab78d1c83c65dceddc
EC2 Default User <ec2-user@ip-172-31-83-212.ec2.internal>
2022-06-30 21:14:46 +0000

    Dump

--- ListComprehensionsHR.py
+++ ListComprehensionsHR.py
@@ -7,4 +7,4 @@ for i in range(x+1):
     for j in range(y+1):
         for k in range(z+1):
             if (i+j+k !=n):
-                print(list([i,j,k]), end = '')
+                print(list([i,j,k]", "), end = '')
