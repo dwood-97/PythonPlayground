6118360237fd6ebfbace8cb23dd2b88846f141a2
EC2 Default User <ec2-user@ip-172-31-83-212.ec2.internal>
2022-07-02 15:55:00 +0000

    ncc

---
+++ NetworkChuckCode.py
@@ -0,0 +1,13 @@
+print("Hello, welcome to Lotus!")
+
+name = input("What is your name?\n\n")
+
+print("Nice to meet you, " + name + ". Thanks for coming in today.\n")
+
+specials = "a Banh Mi, Pho, a Skirt Steak Rice Bowl, and Fried Chicken Rolls"
+
+print(name + ", our specials for the day are " + specials + ".")
+
+order = input("What would you like to order?\n\n")
+
+print("Sounds good " + name + ", we'll have that ready for you in a moment.")
